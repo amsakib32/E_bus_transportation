d5ac0c5ce15b56571b6015f35618f529460f03e9
Rashed Khandoker <rashedrony02@gmail.com>
2021-03-09 00:14:42 +0600

    bus update

--- bus/admin.py
+++ bus/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Bus,Use
 # Register your models here.
+admin.site.register(Bus)
+admin.site.register(Use)
--- bus/views.py
+++ bus/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Bus,Use
 # Create your views here.
+def show_info(request):
+    bus_list=Bus.objects.all()
+    context={
+        'buses':bus_list
+    }
+    return render(request,'bus/show.html',context)
+def use_show_info(request):
+    use_list=Use.objects.all()
+    context={
+        'uses':use_list
+    }
+    return render(request,'bus/use.html',context)

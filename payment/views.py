7a13b4e1455e69299b30b1571c5de4649e96a8b5
SHAON1028 <18101028@uap-bd.edu>
2021-03-14 20:38:14 +0600

    Payment Update

--- payment/views.py
+++ payment/views.py
@@ -1,1 +1,9 @@
-
+from django.shortcuts import render
+from .models import Payment
+# Create your views here.
+def show_info(request):
+    pay_list=Payment.objects.all()
+    context={
+        'pays':pay_list
+    }
+    return render(request,'payment/show.html',context)

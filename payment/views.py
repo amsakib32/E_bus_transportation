0a7a94f38afcd465e297886c04f1dc7751a7c847
SHAON1028 <67582213+SHAON1028@users.noreply.github.com>
2021-04-10 19:53:29 +0600

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Payment
-# Create your views here.
+# Create your  views here.
 def show_info(request):
     pay_list=Payment.objects.all()
     context={

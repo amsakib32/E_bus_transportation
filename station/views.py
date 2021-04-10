f7325245b0290a02918993ae4bc67dff59ba9b9a
SHAON1028 <67582213+SHAON1028@users.noreply.github.com>
2021-04-10 19:57:12 +0600

    Update views.py

--- station/views.py
+++ station/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Station
+# Create   your views here .
+def show_info(request):
+    station_list=Station.objects.all()
+    context={
+        'stations':station_list
+    }
+    return render(request,'station/show.html',context)

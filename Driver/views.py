d19ae09e9c4eff2c2edf0ba7624d20dcbc729791
Rashed Khandoker <rashedrony02@gmail.com>
2021-04-12 01:23:31 +0600

    update driver views

---
+++ Driver/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Driver
+# Create your views here.
+def show_info(request):
+    driver_list=Driver.objects.all()
+    context={
+        'drivers':driver_list
+    }
+    return render(request,'driver/driver.html',context)
